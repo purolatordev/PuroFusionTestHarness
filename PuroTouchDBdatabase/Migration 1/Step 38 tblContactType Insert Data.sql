dd67162476414ec678ed16cb6dbfb24d606ee8df
Scott Cardinale <scott.cardinale@hotmail.com>
2021-08-05 12:48:00 -0400

    1. Added new scripts. 2. Create a PuroTouchDB_Prod to the purofusion test gui.

---
+++ PuroTouchDBdatabase/Migration 1/Step 38 tblContactType Insert Data.sql
@@ -0,0 +1,19 @@
+USE [PuroTouchDB_Prod]
+--USE [PuroTouchDB]
+GO
+SET IDENTITY_INSERT [dbo].[tblContactType] ON 
+GO
+INSERT [dbo].[tblContactType] ([idContactType], [ContactType], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (1, N'Customer Business', N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), 1)
+GO
+INSERT [dbo].[tblContactType] ([idContactType], [ContactType], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (2, N'Customer IT', N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), 1)
+GO
+INSERT [dbo].[tblContactType] ([idContactType], [ContactType], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (3, N'Customer Billing', N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), 1)
+GO
+INSERT [dbo].[tblContactType] ([idContactType], [ContactType], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (4, N'Customer EDI', N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), 1)
+GO
+INSERT [dbo].[tblContactType] ([idContactType], [ContactType], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (5, N'Freight Auditor Programmer', N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), 1)
+GO
+INSERT [dbo].[tblContactType] ([idContactType], [ContactType], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (6, N'Freight Auditor Billing', N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), N'Michele.Kennedy', CAST(N'2021-02-15T00:00:00.000' AS DateTime), 1)
+GO
+SET IDENTITY_INSERT [dbo].[tblContactType] OFF
+GO
---
+++ PuroTouchDBdatabase/Migration 1/Step 39 tblCommunicationMethod Insert Data.sql
@@ -0,0 +1,18 @@
+USE [PuroTouchDB_Prod]
+GO
+SET IDENTITY_INSERT [dbo].[tblCommunicationMethod] ON 
+GO
+INSERT [dbo].[tblCommunicationMethod] ([idCommunicationMethod], [CommunicationMethod], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (0, N'None', NULL, NULL, N'Scott.Cardinale', CAST(N'2021-04-16T10:51:55.257' AS DateTime), 0)
+GO
+INSERT [dbo].[tblCommunicationMethod] ([idCommunicationMethod], [CommunicationMethod], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (1, N'FTP Internal', N'Admin.ITManager', CAST(N'2017-11-08T11:40:52.970' AS DateTime), N'Scott.Cardinale', CAST(N'2021-04-16T10:51:55.303' AS DateTime), 1)
+GO
+INSERT [dbo].[tblCommunicationMethod] ([idCommunicationMethod], [CommunicationMethod], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (2, N'FTP External', N'Admin.ITManager', CAST(N'2017-11-08T11:40:48.827' AS DateTime), N'Scott.Cardinale', CAST(N'2021-04-16T10:51:55.360' AS DateTime), 1)
+GO
+INSERT [dbo].[tblCommunicationMethod] ([idCommunicationMethod], [CommunicationMethod], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (3, N'Email', N'Admin.ITManager', CAST(N'2017-11-08T11:40:45.133' AS DateTime), N'Scott.Cardinale', CAST(N'2021-04-16T10:51:55.400' AS DateTime), 1)
+GO
+INSERT [dbo].[tblCommunicationMethod] ([idCommunicationMethod], [CommunicationMethod], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (4, N'AS2', N'Admin.ITManager', CAST(N'2017-11-08T11:40:40.653' AS DateTime), N'Scott.Cardinale', CAST(N'2021-04-16T10:51:55.443' AS DateTime), 1)
+GO
+INSERT [dbo].[tblCommunicationMethod] ([idCommunicationMethod], [CommunicationMethod], [UpdatedBy], [UpdatedOn], [CreatedBy], [CreatedOn], [ActiveFlag]) VALUES (5, N'VAN', N'Admin.ITManager', CAST(N'2017-11-08T11:40:56.293' AS DateTime), N'Scott.Cardinale', CAST(N'2021-04-16T10:51:55.487' AS DateTime), 1)
+GO
+SET IDENTITY_INSERT [dbo].[tblCommunicationMethod] OFF
+GO
