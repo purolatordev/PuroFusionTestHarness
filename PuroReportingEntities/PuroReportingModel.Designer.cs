7b9b96e6f1af819d9fa14e1dba34810d2e4f8c19
Scott Cardinale <redbird510@hotmail.com>
2023-12-05 16:38:46 -0500

    Upgraded

--- PuroReportingEntities/PuroReportingModel.Designer.cs
+++ PuroReportingEntities/PuroReportingModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Src\PuroFusion\PuroFusionSolution\PuroReportingEntities\PuroReportingModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\scott.cardinale\OneDrive - Purolator Inc\Src\PuroFusion\PuroFusionTestHarness\PuroReportingEntities\PuroReportingModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
